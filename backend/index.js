a777eb7fca0891abecfbae02cbe827c1ff0e6964
Melker Thunberg <melker.thunberg@gmail.com>
2024-04-30 12:22:44 +0200

    started login backend

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,14 @@
-const express = require("express");
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const router = require('./routes/router')
+import express from "express";
+import cors from 'cors';
+import router from './routes/router.js';
+import pkg from 'body-parser';
+const { json, urlencoded } = pkg;
 
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(json());
+app.use(urlencoded({ extended: false }));
 
 const corseOptions = {
     origin: '*',
@@ -19,6 +20,8 @@ app.use(cors(corseOptions));
 app.use('/', router)
 
 const port = 4000
+// eslint-disable-next-line no-unused-vars
 const server = app.listen(port, () => {
     console.log(`Server started on port http://localhost:${port}`)
 })
+
