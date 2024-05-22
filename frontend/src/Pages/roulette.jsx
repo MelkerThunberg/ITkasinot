f45edaedc14e66faae2ed7e091d728a849c744cd
Melker Thunberg <melker.thunberg@gmail.com>
2024-05-23 00:16:20 +0200

    small fix h1

--- frontend/src/Pages/roulette.jsx
+++ frontend/src/Pages/roulette.jsx
@@ -3,7 +3,8 @@ import React from "react";
 export default function Roulette() {
   return (
     <div>
-      <h1 id="unusedh1">Plinko</h1>
+      <h1 id="unusedh1">Roulette</h1>
+      <h2>Work in progress </h2>
     </div>
   );
 }
